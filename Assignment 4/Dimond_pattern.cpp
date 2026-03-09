5a6608de46b9dc2d3a406c596c7454f774c7e2ec
Sumit Kadam <sumitkadam.dev@gmail.com>
2026-03-09 15:49:36 +0530

    gg bolte

---
+++ Assignment 4/Dimond_pattern.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows for the diamond pattern: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j < n; j++)
+            cout << " ";
+        for (int j = 1; j <= i; j++)
+            cout << j;
+        for (int j = i - 1; j >= 1; j--)
+            cout << j;
+        cout << endl;
+    }
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = n; j > i; j--)
+            cout << " ";
+
+        for (int j = 1; j <= i; j++)
+            cout << j;
+
+        for (int j = i - 1; j >= 1; j--)
+            cout << j;
+
+        cout << endl;
+    }
+    return 0;
+}
